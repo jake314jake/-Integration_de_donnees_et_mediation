1621120f92d7c05ba7b01a65329dbdccc578bae1
jake314jake <jakeamberson314@gmail.com>
2023-03-31 16:05:44 +0100

    add sql query (generate data)

---
+++ sqlQuery/dataQuery/sourceOne(S1)/Employe.sql
@@ -0,0 +1,21 @@
+--Emp(EmpNo, Name, data_naissance, Email)
+insert into Emp (EmpNo, Name, Email, data_naissance) values (31, 'Vidovic Parradye', 'vparradyeu@ocn.ne.jp', '4/27/1983');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (32, 'Anetta Kroger', 'akrogerv@flavors.me', '12/2/1980');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (33, 'Ayn Rentcome', 'arentcomew@dagondesign.com', '1/29/1977');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (34, 'Megan Sillis', 'msillisx@diigo.com', '2/22/1979');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (35, 'Gabbie Bedinn', 'gbedinny@rambler.ru', '12/20/1977');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (36, 'Jenni Alpin', 'jalpinz@ecoNameist.com', '6/2/1978');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (37, 'Inigo Cheale', 'icheale10@Email.ru', '10/30/1988');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (38, 'Heriberto Basler', 'hbasler11@zimbio.com', '8/30/1984');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (39, 'Boony Ghost', 'bghost12@walmart.com', '9/20/1981');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (40, 'Guinna Breslin', 'gbreslin13@baidu.com', '7/15/1976');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (41, 'Lewiss Jardine', 'ljardine14@weather.com', '7/14/1980');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (42, 'Cookie Goodbarr', 'cgoodbarr15@wordpress.org', '11/26/1983');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (43, 'Margery Filpi', 'mfilpi16@rediff.com', '5/17/1982');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (44, 'Tomasine Sowerbutts', 'tsowerbutts17@digg.com', '10/12/1984');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (45, 'Sonya Minchinton', 'sminchinton18@gravatar.com', '8/13/1983');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (46, 'Merrie Le land', 'mle19@163.com', '7/19/1987');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (47, 'Davy Brunsdon', 'dbrunsdon1a@ask.com', '5/12/1985');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (48, 'Francklyn Barnham', 'fbarnham1b@github.io', '11/16/1986');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (49, 'Agatha Tute', 'atute1c@themeforest.net', '1/19/1982');
+insert into Emp (EmpNo, Name, Email, data_naissance) values (50, 'Vance Matheson', 'vmatheson1d@elegantthemes.com', '4/15/1976');
